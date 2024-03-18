6ae11fe5aba0d6399ef1d2763e449225594b4d72
mahmoudellabban <mahmoud.ellabban.7@gmail.com>
2024-03-19 01:38:12 +0200

    fetching data

--- src/Components/Exercises/Exercises.js
+++ src/Components/Exercises/Exercises.js
@@ -1,11 +1,60 @@
-import React from 'react'
-import "./Exercises.css"
+import React, { useState, useEffect } from "react";
+import { exercisesOptions, fetchData } from "../../utils/fetchData";
+import "./Exercises.css";
 const Exercises = () => {
+  const [search, setSearch] = useState("");
+  const [exercises, setExercises] = useState([]);
+  const [bodyParts, setBodyParts] = useState([]);
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        exercisesOptions
+      );
+      setBodyParts(["all", ...bodyPartsData]);
+    };
+    fetchExercisesData()
+  },[]);
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises",
+        exercisesOptions
+      );
+      const searchedExercises = exercisesData.filter(
+        (exercise) =>
+          exercise.name.toLowerCase().includes(search) ||
+          exercise.target.toLowerCase().includes(search) ||
+          exercise.equipment.toLowerCase().includes(search) ||
+          exercise.bodyPart.toLowerCase().includes(search)
+      );
+      setSearch("");
+      setExercises(searchedExercises);
+    }
+  };
   return (
-    <div className='container'>
-      hhh
-    </div>
-  )
-}
+    <section className="container" id="exercises">
+      <div className="search">
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          type="text"
+          placeholder="Search here..."
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <div className="parts">
+        {bodyParts.map((item) => (
+          <div key={item.id || item} className="part">
+            {item}
+          </div>
+        ))}
+      </div>
+      <div className="wrapper">
+        <div className="card">exercise card</div>
+      </div>
+    </section>
+  );
+};
 
-export default Exercises
+export default Exercises;
